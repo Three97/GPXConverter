23337e16d5199d9eb7f5d13a469910a9bda09ebf
Eric Davis <eric.davis74@gmail.com>
2016-09-28 14:39:32 -0400

    WIP

--- GPXConverter/DeviceWorkout.Gpx.cs
+++ GPXConverter/DeviceWorkout.Gpx.cs
@@ -6,6 +6,7 @@ using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
 using System.Text;
+using System.Xml.XPath;
 //using Dynastream;
 
 /// <summary>
@@ -13,6 +14,13 @@ using System.Text;
 /// </summary>
 public partial class DeviceFile
 {
+    private enum NamespaceType
+    {
+        GPX,
+        TrackPointExtensions,
+        GPXExtensions
+    }
+
     const double _eQuatorialEarthRadius = 6378.1370D;
     const double _d2r = (Math.PI / 180D);
 
@@ -74,10 +82,30 @@ public partial class DeviceFile
     private static DeviceFile FromGPXV1_Base(XmlDocument Doc)
     {
         // TODO: How do you handle variable namespace prefixes???
+        var validNamespaceDict = new Dictionary<NamespaceType, string>
+        {
+            { NamespaceType.GPX,  "http://www.topografix.com/GPX/1/1" },
+            { NamespaceType.TrackPointExtensions, "http://www.garmin.com/xmlschemas/TrackPointExtension/v1" },
+            { NamespaceType.GPXExtensions, "http://www.garmin.com/xmlschemas/GpxExtensions/v3" }
+        };
+
+        var namespacePrefixDict = new Dictionary<NamespaceType, string>();
+
+        var nav = Doc.CreateNavigator();
+        nav.MoveToFollowing(XPathNodeType.Element);
+        var namespaces = nav.GetNamespacesInScope(XmlNamespaceScope.Local)
+                        .Join(validNamespaceDict, o => o.Value, i => i.Value, (i, o) => 
+                        new
+                        {
+                            Type = o.Key,
+                            Prefix = string.IsNullOrWhiteSpace(i.Key) ? "gpx" : i.Key,
+                            Url = o.Value })
+                        .ToList();
+
+        namespaces.ForEach(x => namespacePrefixDict.Add(x.Type, x.Prefix));
+        
         var ns = new XmlNamespaceManager(Doc.NameTable);
-        ns.AddNamespace("gpx", "http://www.topografix.com/GPX/1/1");
-        ns.AddNamespace("gpxtpx", "http://www.garmin.com/xmlschemas/TrackPointExtension/v1");
-        ns.AddNamespace("gpxx", "http://www.garmin.com/xmlschemas/GpxExtensions/v3");
+        namespaces.ForEach(x => ns.AddNamespace(x.Prefix, x.Url));
         XMLParserHelper.Manager = ns;
 
         var CurrentAuthor = new DeviceAuthor();
@@ -90,7 +118,7 @@ public partial class DeviceFile
         File.Author = CurrentAuthor;
         DateTimeOffset dtoFileTime;
         DateTime? fileTime = null;
-        var fileTimeString = XMLParserHelper.SelectSingleTextString(Doc, "/gpx:gpx/gpx:metadata/gpx:time");
+        var fileTimeString = XMLParserHelper.SelectSingleTextString(Doc, $"/{namespacePrefixDict[NamespaceType.GPX]}:gpx/{namespacePrefixDict[NamespaceType.GPX]}:metadata/{namespacePrefixDict[NamespaceType.GPX]}:time");
         if (!string.IsNullOrWhiteSpace(fileTimeString) && DateTimeOffset.TryParse(fileTimeString, out dtoFileTime))
         {
             // TODO: FSSecurity.Current.ToUserTime ???
@@ -103,9 +131,9 @@ public partial class DeviceFile
             var CurrentActivity = new DeviceActivity();
             File.Activities.Add(CurrentActivity);
 
-            CurrentActivity.Id = XMLParserHelper.SelectSingleTextString(Activity, "gpx:name");
+            CurrentActivity.Id = XMLParserHelper.SelectSingleTextString(Activity, $"{namespacePrefixDict[NamespaceType.GPX]}:name");
             CurrentActivity.ActivityTime = fileTime;
-            CurrentActivity.Sport = XMLParserHelper.SelectSingleTextString(Activity, "gpx:type", "Unknown");
+            CurrentActivity.Sport = XMLParserHelper.SelectSingleTextString(Activity, $"{namespacePrefixDict[NamespaceType.GPX]}:type", "Unknown");
 
             var CurrentCreator = new DeviceCreator();
             CurrentCreator.Name = "???";
@@ -118,13 +146,48 @@ public partial class DeviceFile
             var Lap = new DeviceLap();
             CurrentActivity.Laps.Add(Lap);
 
-            var TrackPoints = Activity.SelectNodes("gpx:trkseg/gpx:trkpt", ns);
+            // TODO: Calculate these values???
+            Lap.StartSeconds = null;
+            Lap.Time = null;
+            Lap.Distance = null;
+            Lap.SpeedAvg = null;
+            Lap.SpeedMax = null;
+            Lap.Calories = null;
+            Lap.RPMAvg = null;
+            Lap.RPMMax = null;
+            Lap.WattsAvg = null;
+            Lap.WattsMax = null;
+
+            var TrackPoints = Activity.SelectNodes($"{namespacePrefixDict[NamespaceType.GPX]}:trkseg/{namespacePrefixDict[NamespaceType.GPX]}:trkpt", ns);
             foreach (XmlNode Point in TrackPoints)
             {
                 var CurrentPoint = new DevicePoint();
                 Lap.Track.Add(CurrentPoint);
-                //var node = Point.SelectSingleNode()
+
+                var pointTime = XMLParserHelper.SelectSingleTextDateTime(Point, $"{namespacePrefixDict[NamespaceType.GPX]}:time", null);
+                CurrentPoint.StartSeconds = null; // TODO: Calculate from pointTimes?
+                CurrentPoint.Latitude = XMLParserHelper.SelectSingleAttributeDecimal(Point, "lat", null);
+                CurrentPoint.Longitude = XMLParserHelper.SelectSingleAttributeDecimal(Point, "lon", null);
+                CurrentPoint.Altitude = XMLParserHelper.SelectSingleTextDecimal(Point, $"{namespacePrefixDict[NamespaceType.GPX]}:ele", null);
+                CurrentPoint.Distance = null;
+                CurrentPoint.HR = XMLParserHelper.SelectSingleTextInt(Point, $"{namespacePrefixDict[NamespaceType.GPX]}:extensions/{namespacePrefixDict[NamespaceType.TrackPointExtensions]}:TrackPointExtension/{namespacePrefixDict[NamespaceType.TrackPointExtensions]}:hr", null);
+                CurrentPoint.RPM = null;
+                CurrentPoint.Temp = XMLParserHelper.SelectSingleTextInt(Point, $"{namespacePrefixDict[NamespaceType.GPX]}:extensions/{namespacePrefixDict[NamespaceType.TrackPointExtensions]}:TrackPointExtension/{namespacePrefixDict[NamespaceType.TrackPointExtensions]}:atemp", null);
+                CurrentPoint.CAD = XMLParserHelper.SelectSingleTextInt(Point, $"{namespacePrefixDict[NamespaceType.GPX]}:extensions/{namespacePrefixDict[NamespaceType.TrackPointExtensions]}:TrackPointExtension/{namespacePrefixDict[NamespaceType.TrackPointExtensions]}:cad", null); 
+                // TODO: Multiply CAD result x 2?
+                CurrentPoint.Speed = null; // Recalculate this value later?
+                CurrentPoint.Watts = null;
             }
+
+            // Calculate HRAvg and HRMax from lap's collection of HR points...
+            var hrPoints = Lap.Track.Where(t => t.HR != null).Select(x => x.HR).ToList();
+            Lap.HeartRateMax = hrPoints.Max();
+            Lap.HeartRateAvg = (int?)hrPoints.Average();
+
+            // Calculate CADAvg and CADMax from lap's collection of CAD points...
+            var cadPoints = Lap.Track.Where(t => t.CAD != null).Select(x => x.CAD).ToList();
+            Lap.CADMax = cadPoints.Any() ? cadPoints.Max() : null;
+            Lap.CADAvg = cadPoints.Any() ? (int?)cadPoints.Average() : null;
         }
 
         return File;
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace TestConsole
 {
@@ -10,7 +13,8 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            var gpxFile = @"C:\Temp\GPXFiles\activity_1358503471.gpx";
+            //var gpxFile = @"C:\DEV\GitHub\GPXConverter\GPXFiles\activity_1358503471.gpx";
+            var gpxFile = @"C:\DEV\GitHub\GPXConverter\GPXFiles\Evening_Run.gpx";
             var test = DeviceFile.FromGpx(gpxFile);
 
             Console.WriteLine("\n\nPress any key to continue...");
